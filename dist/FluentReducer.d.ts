9b129f0d5ba7dcfe4f1c48753cc275a369056424
frontainer <frontainer@gmail.com>
2020-06-01 16:47:55 +0900

    定義反映漏れ修正

--- dist/FluentReducer.d.ts
+++ dist/FluentReducer.d.ts
@@ -14,5 +14,5 @@ export declare class FluentReducer<InS> {
     reducer: (state: InS, action: any) => Readonly<InS>;
     private _caseWithAction;
     sync<P = void>(type: string, handler: IHandler<InS, P>): IActionCreator<P>;
-    async<Param, Result = any, Err = Error>(type: string, handler: IAsyncHandler<InS, Param, Result>, handlers?: Partial<IAsyncHandlers<InS, Param, Result, Err>>): (param: Param) => AsyncActionCreator<InS, Param, Result, Err>;
+    async<Param = void, Result = void, Err = Error>(type: string, handler: IAsyncHandler<InS, Param, Result>, handlers?: Partial<IAsyncHandlers<InS, Param, Result, Err>>): (param: Param) => AsyncActionCreator<InS, Param, Result, Err>;
 }
